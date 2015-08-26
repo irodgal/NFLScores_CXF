b393629917ac237b8bc0e3c909a30c0405d25ac3
Iván R. Galicia <irodgal@gmail.com>
2015-08-26 16:49:13 +0200

    Unión con el proyecto en casa

--- NFLScores/src/main/java/com/irodgal/App.java
+++ NFLScores/src/main/java/com/irodgal/App.java
@@ -7,92 +7,41 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 
-	// @SuppressWarnings("static-access")
-	public String getNFLWebPageOutput() throws Exception {
+	public String getNFLWebOutputAsString() throws Exception {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-		// InputStream instream = null;
-
 		try {
-			HttpGet httpget = new HttpGet("http://www.nfl.com/");
+			HttpGet httpget = new HttpGet("http://www.nfl.com");
 
-			// System.out.println("Executing request " +
-			// httpget.getRequestLine());
 			CloseableHttpResponse response = httpclient.execute(httpget);
 			try {
-				// System.out.println("----------------------------------------");
-				// System.out.println(response.getStatusLine());
-
 				// Get hold of the response entity
 				HttpEntity entity = response.getEntity();
 
-				// System.out.println(EntityUtils.toString(entity));
 				return EntityUtils.toString(entity);
-
-				// If the response does not enclose an entity, there is no need
-				// to bother about connection release
-				/*
-				 * if (entity != null) { instream = entity.getContent(); try {
-				 * instream.read(); // do something useful with the response
-				 * 
-				 * } catch (IOException ex) { // In case of an IOException the
-				 * connection will be // released // back to the connection
-				 * manager automatically throw ex; } finally { // Closing the
-				 * input stream will trigger connection // release
-				 * instream.close(); } }
-				 */
 			} finally {
 				response.close();
 			}
 		} finally {
 			httpclient.close();
 		}
-
-		// return instream;
 	}
 
-	public String getNFLWebScoresOutput() throws Exception {
-		// http://www.nfl.com/liveupdate/scorestrip/ss.xml?random=1440494159650
-
+	public String getNFLScorestripOutputAsString() throws Exception {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-
 		try {
-			HttpGet httpget = new HttpGet(
-					"http://www.nfl.com/liveupdate/scorestrip/ss.xml");
+			HttpGet httpget = new HttpGet("http://www.nfl.com/liveupdate/scorestrip/ss.xml");
 
-			// System.out.println("Executing request " +
-			// httpget.getRequestLine());
 			CloseableHttpResponse response = httpclient.execute(httpget);
 			try {
-				// System.out.println("----------------------------------------");
-				// System.out.println(response.getStatusLine());
-
 				// Get hold of the response entity
 				HttpEntity entity = response.getEntity();
 
-				// System.out.println(EntityUtils.toString(entity));
 				return EntityUtils.toString(entity);
-
-				// If the response does not enclose an entity, there is no need
-				// to bother about connection release
-				/*
-				 * if (entity != null) { instream = entity.getContent(); try {
-				 * instream.read(); // do something useful with the response
-				 * 
-				 * } catch (IOException ex) { // In case of an IOException the
-				 * connection will be // released // back to the connection
-				 * manager automatically throw ex; } finally { // Closing the
-				 * input stream will trigger connection // release
-				 * instream.close(); } }
-				 */
 			} finally {
 				response.close();
 			}
--- NFLScores/src/test/java/com/irodgal/AppTest.java
+++ NFLScores/src/test/java/com/irodgal/AppTest.java
@@ -1,49 +1,48 @@
 package com.irodgal;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-	/*
-	 * public static void main(String[] args) {
-	 * JUnitCore.main("grupoPrueba.artifactPrueba.App"); }
-	 */
-	@Test
-	public void getNFLWebOutputTest() {
-		App app = new App();
-		String output = "";
-		try {
-			output = app.getNFLWebPageOutput();
 
+	private static App app;
+
+	@BeforeClass
+	public static void setUp() {
+		app = new App();
+	}
+
+	@Test
+	public void getNFLWebOutputAsStringTest() {
+		String output;
+		try {
+			output = app.getNFLWebOutputAsString();
+			// assertThat(output, equalTo("hola2"));
+			assertThat(output,
+					containsString("<title>NFL.com - Official Site of the National Football League</title>"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// System.out.println(output);
-		// assertThat(output, equalTo("hola"));
-		assertTrue(output.contains("<title>NFL.com - Official Site of the National Football League</title>"));
-		assertTrue(output.contains("hd-scorestrip"));
-		assertTrue(output.contains("yui3-scorestrip-tile-clock"));
-
 	}
 
 	@Test
-	public void getNFLWebScoresOutputTest() {
-		App app = new App();
-		String output = "";
+	public void getNFLScorestripOutputAsStringTest() {
+		String output;
 		try {
-			output = app.getNFLWebScoresOutput();
-
+			output = app.getNFLScorestripOutputAsString();
+			assertThat(output, containsString("<ss>"));
+			assertThat(output, containsString("</ss>"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(output);
-		// assertThat(output, equalTo("hola"));
-
 	}
+
 }
